06547a5632217a2a957d7cea0641f27e0d92465d
Zacharie <zacharie21000@gmail.com>
2026-08-24 12:21:43 +0200

    feat: initialiser le catalogue des intérêts

---
+++ app/Models/InterestSetting.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property int $max_selections
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
+#[Fillable(['max_selections'])]
+class InterestSetting extends Model
+{
+    public static function current(): self
+    {
+        return self::query()->firstOrCreate(['id' => 1], ['max_selections' => 5]);
+    }
+
+    /** @return array<string, string> */
+    protected function casts(): array
+    {
+        return ['max_selections' => 'integer'];
+    }
+}
