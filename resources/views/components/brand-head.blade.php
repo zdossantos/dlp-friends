db91ac73490638a742b4169bfd113e2abb12ba31
Zacharie <zacharie21000@gmail.com>
2026-09-26 20:30:57 +0200

    feat(brand): colorer le logo et moderniser le favicon (#214)

---
+++ resources/views/components/brand-head.blade.php
@@ -0,0 +1,5 @@
+<meta name="theme-color" media="(prefers-color-scheme: light)" content="hsl(32 40% 98%)">
+<meta name="theme-color" media="(prefers-color-scheme: dark)" content="hsl(258 30% 8%)">
+<link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any">
+<link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16 32x32 48x48">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" type="image/png" sizes="180x180">
